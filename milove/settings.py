59b91063e68e1c55034b6403c610fd1db287dd31
Richard Chien <richardchienthebest@gmail.com>
2017-07-24 00:09:26 +0800

    Add translation for app name

--- milove/settings.py
+++ milove/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'UTC'
 
--- milove/shop/__init__.py
+++ milove/shop/__init__.py
@@ -1,1 +1,3 @@
 from . import signal_handlers  # global signal handlers
+
+default_app_config = 'milove.shop.apps.ShopConfig'
